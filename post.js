d4c27c4c3c3ff5b0906d96376692d6ce382b861d
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-03 22:02:20 -0500

    moved submit to initEvents

--- post.js
+++ post.js
@@ -4,6 +4,9 @@ var Post = {
         Post.initEvents();
     },
     initStyling: function() {},
+    initEvents: function() {
+      $('form').on('submit', Post.createPost);
+    },
     createPost: function(event) {
         var newPost = new Message($('textarea').val());
         $('textarea').val('');
@@ -15,7 +18,7 @@ var Post = {
                 console.log(response);
                 var postHTML = chatPage.messageTemplate(newPost);
                 $('.messages').append(postHTML);
-                $('form').on('submit', Post.createPost);
+                
             }
         })
     },
